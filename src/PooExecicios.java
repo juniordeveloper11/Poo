5b6dab272e725d5ebab6fd0440b7ac129f44c380
juniordeveloper11 <89851068+juniordeveloper11@users.noreply.github.com>
2022-05-17 21:48:30 -0300

    Initial commit

---
+++ src/PooExecicios.java
@@ -0,0 +1,10 @@
+import java.util.Locale;
+
+public class PooExecicios {
+    public static void main(String[] args) {
+        double y = 10.345;
+        System.out.printf( "%4f%n",y);
+        System.out.println(" Resultado = " + y + "Metros");
+        Locale.setDefault(Locale.US);
+    }
+}
